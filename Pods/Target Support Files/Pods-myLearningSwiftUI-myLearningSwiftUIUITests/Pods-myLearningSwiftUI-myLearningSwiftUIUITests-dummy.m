0b003b2fb86e271ac6c59f5d9397301c4cb57a91
judagonsa <judagonsa8@gmail.com>
2021-09-16 11:39:54 -0500

    Create new project

---
+++ Pods/Target Support Files/Pods-myLearningSwiftUI-myLearningSwiftUIUITests/Pods-myLearningSwiftUI-myLearningSwiftUIUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_myLearningSwiftUI_myLearningSwiftUIUITests : NSObject
+@end
+@implementation PodsDummy_Pods_myLearningSwiftUI_myLearningSwiftUIUITests
+@end
---
+++ Pods/Target Support Files/Pods-myLearningSwiftUI/Pods-myLearningSwiftUI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_myLearningSwiftUI : NSObject
+@end
+@implementation PodsDummy_Pods_myLearningSwiftUI
+@end
