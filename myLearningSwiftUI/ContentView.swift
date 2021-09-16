0696f8eff7ac8179415607abb8640971a19282c2
judagonsa <judagonsa8@gmail.com>
2021-09-16 11:46:11 -0500

    Touch .gitignore

--- myLearningSwiftUI/ContentView.swift
+++ myLearningSwiftUI/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, swiftUI init")
+        Text("Hello, my learninig Judagonsa")
             .padding()
     }
 }
